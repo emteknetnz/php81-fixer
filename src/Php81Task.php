5815be442dd7154a6e6faf52a0c2c5ce0bd08c24
Steve Boyd <emteknetnz@gmail.com>
2022-03-31 19:04:15 +1300

    Use cast and ternaryy to rewrite code

--- src/Php81Task.php
+++ src/Php81Task.php
@@ -1,9 +1,17 @@
 <?php
 
-use SilverStripe\Dev\BuildTask;
 use PhpParser\Error;
+use PhpParser\Lexer;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Expr\Variable;
 use PhpParser\NodeDumper;
 use PhpParser\ParserFactory;
+use PhpParser\Node\Stmt\Class_;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Expression;
+use SilverStripe\Dev\BuildTask;
 
 class Php81Task extends BuildTask
 {
@@ -19,22 +27,41 @@ class Php81Task extends BuildTask
 
         class MyClass
         {
-            public function myMethod($foo)
+            public function secondMethod($foo)
             {
                 $str = 'xyz';
                 $a = str_replace('a', 'b', 'c');
                 $b = str_replace('x', 'y', $str);
-                str_replace('g', 'h', $str);
+                preg_match('/g/', $str);
+            }
+
+            public function myMethod($foo)
+            {
+                $str = 'xyz';
+                $a = str_replace('a', 'b', 'c');
+                $b = str_replace('x', 'y', $xstr);
+                preg_match('/g/', $xstr);
             }
         }
         CODE;
     }
 
-    private function getAst(): array
+    private function getAst(string $code): array
     {
-        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        $lexer = new Lexer([
+            'usedAttributes' => [
+                'comments',
+                //'startLine',
+                //'endLine',
+                //'startTokenPos',
+                //'endTokenPos',
+                'startFilePos',
+                'endFilePos'
+            ]
+        ]);
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7, $lexer);
         try {
-            $ast = $parser->parse($this->getCode());
+            $ast = $parser->parse($code);
         } catch (Error $error) {
             echo "Parse error: {$error->getMessage()}\n";
             die;
@@ -48,9 +75,84 @@ class Php81Task extends BuildTask
         echo $dumper->dump($ast) . "\n";
     }
 
+    private function getMethods(Class_ $class): array
+    {
+        return array_filter($class->stmts, fn($v) => $v instanceof ClassMethod);
+    }
+
+    private function recursiveAddFuncCalls(Expr $expr, array &$funcCalls): void
+    {
+        if ($expr instanceof FuncCall) {
+            $funcCalls[] = $expr;
+        }
+        if (property_exists($expr, 'expr')) {
+            $this->recursiveAddFuncCalls($expr->expr, $funcCalls);
+        }
+    }
+
+    private function getFuncCalls(ClassMethod $method): array
+    {
+        $funcCalls = [];
+        /** @var Expression $expression */
+        foreach ($method->stmts as $expression) {
+            $this->recursiveAddFuncCalls($expression->expr, $funcCalls);
+        }
+        return $funcCalls;
+    }
+
+    private function getFuncCallConfig(string $name)
+    {
+        $a = [
+            'preg_match' => [2 => 'cast'],
+            'str_replace' => [3 => 'ternary'] // as in string cast the 3rd arg
+        ];
+        return $a[$name] ?? [];
+    }
+
     private function test()
     {
-        $ast = $this->getAst();
-        print_r($ast);
+        $code = $this->getCode();
+        $ast = $this->getAst($code);
+        $classes = array_filter($ast, fn($v) => $v instanceof Class_);
+        foreach ($classes as $class) {
+            foreach ($this->getMethods($class) as $method) {
+                $funcCalls = $this->getFuncCalls($method);
+                $funcCalls = array_reverse($funcCalls);
+                foreach ($funcCalls as $funcCall) {
+                    $name = $funcCall->name->parts[0];
+                    $config = $this->getFuncCallConfig($name);
+                    if (empty($config)) {
+                        continue;
+                    }
+                    $config = array_reverse($config, true);
+                    foreach ($config as $argNum => $what) {
+                        $a = $argNum - 1;
+                        /** @var Arg $arg */
+                        $arg = $funcCall->args[$a];
+                        if (!($arg->value instanceof Variable)) {
+                            continue;
+                        }
+                        /** @var Variable $variable */
+                        $variable = $arg->value;
+                        if ($what == 'cast') {
+                            $code = implode('', [
+                                substr($code, 0, $variable->getStartFilePos()),
+                                '(string) ',
+                                substr($code, $variable->getStartFilePos()),
+                            ]);
+                        } elseif ($what == 'ternary') {
+                            echo $variable->getEndFilePos() . "\n";
+                            $code = implode('', [
+                                substr($code, 0, $variable->getEndFilePos() + 1),
+                                " ?: ''",
+                                substr($code, $variable->getEndFilePos() + 1),
+                            ]);
+                        }
+                    }
+                }
+            }
+        }
+        echo $code;
+        // print_r($ast);
     }
 }
